95d640432e43ba4c40e86e5bdab3cd7f73925ae8
SundayNwovu <n.sunday@zarttech.com>
2024-08-10 22:51:05 +0200

    Removed creating chromadb" " " "

--- main3.py
+++ main3.py
@@ -179,7 +179,7 @@ def create_vector_database():
     persist_directory = "chroma_db_llamaparse1"
     collection_name = "trustbreed"
     embed_model = FastEmbedEmbeddings(model_name="BAAI/bge-base-en-v1.5")
-    #embed_model = OpenAIEmbeddings(model='text-embedding-3-large')
+    embed_model = OpenAIEmbeddings(model='text-embedding-3-large')
     vectorstore = Chroma(
         #embedding_function=embed_model,
         persist_directory=persist_directory,
@@ -201,7 +201,8 @@ def create_vector_database():
 
 chat_model = ChatAnthropic(model="claude-3-5-sonnet-20240620", api_key=anthropic_api_key, max_tokens=400)
 
-vs, embed_model = create_vector_database()
+#vs, embed_model = create_vector_database()
+embed_model = FastEmbedEmbeddings(model_name="BAAI/bge-base-en-v1.5")
 vectorstore = Chroma(embedding_function=embed_model, persist_directory="chroma_db_llamaparse1", collection_name="trustbreed")
 retriever = vectorstore.as_retriever(search_kwargs={'k': 5})  # Adjusted to top 5 for more precise results
 
